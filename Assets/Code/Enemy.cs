f0a2d4c87fa4bfde96066894c8950406d1ba7846
Harrison Short <work.harrisonshort@gmail.com>
2018-05-10 16:57:28 +1000

    Finished refactoring the way projectiles and dying works (for enemies and player)

---
+++ Assets/Code/Enemy.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Class for a single enemy, to enable dying and dropping powerups
+/// </summary>
+public class Enemy : MonoBehaviour, IKillable
+{
+    public void Die()
+    {
+        Destroy(gameObject);
+        GameController.instance.IncreaseScore();
+        // Play animation
+
+        // Determine possibility of dropping item and drop item if it works
+
+
+    }
+
+    /// <summary>
+    /// Determines the likelihood of an enemy dropping a power up
+    /// when killed.
+    /// </summary>
+    private void DetermineChanceOfDroppingPowerUp(GameObject enemyKilled)
+    {
+        //if (Random.Range(0f, 1f) > chanceOfDrop)
+        //{
+        //    EnemyWaveController enemy = enemyKilled.GetComponent<EnemyWaveController>();
+        //    print("test");
+        //    if (enemy != null)
+        //    {
+        //        enemy.DropPowerUp();
+        //        print("test2");
+
+        //    }
+        //}
+    }
+}
