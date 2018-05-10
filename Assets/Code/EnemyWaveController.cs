9610048339e22cd8161f8cf3263f5352bc1211f4
Harrison Short <work.harrisonshort@gmail.com>
2018-05-10 22:57:35 +1000

    Spawning waves from JSON file now works as expected

--- Assets/Code/EnemyWaveController.cs
+++ Assets/Code/EnemyWaveController.cs
@@ -25,10 +25,31 @@ public class EnemyWaveController : MonoBehaviour
     [Tooltip("The rate at which the enemy can shoot missiles")]
     private float missileRate = 0.99f;
 
+    [Header("Spawn Parameters")]
+    [SerializeField]
+    [Tooltip("The spawn points in the game, exposed in inspector for convenience")] // TODO: Find spawn points via code
+    private GameObject[] spawnPoints;
+    [SerializeField]
+    private GameObject enemyToSpawn;
+    [SerializeField]
+    [Tooltip("The physical distance between waves")]
+    private float distanceBetweenWaves = 1.5f;
+    private int enemiesToSpawn;
+    private int wavesToSpawn;
+
     void Start ()
     {
         InvokeRepeating("MoveEnemyWave", 0.1f, enemyWaveSpeed);
         enemyWaveTransform = GetComponent<Transform>();
+
+        LimitEnemiesAndWavesToSpawn();
+        SpawnWaves();
+
+        print(enemiesToSpawn + " " + wavesToSpawn);
+            //for (int i = 0; i < transform.childCount; i++)
+        //{
+        //    spawnPoints[i] = transform.GetChild(i).gameObject;
+        //}
     }
     
     /// <summary>
@@ -42,7 +63,7 @@ public class EnemyWaveController : MonoBehaviour
         foreach (Transform enemy in enemyWaveTransform)
         {
             // Move downward and reverse movement when wave hits side of screen
-            if (enemy.position.x < -7.5f || enemy.position.x > 7.5f)
+            if (enemy.position.x < -7.5f || enemy.position.x > 7.5f) // TODO: get rid of magic numbers
             {
                 enemyWaveDistance = -enemyWaveDistance;
                 enemyWaveTransform.position += Vector3.down * 0.5f;
@@ -62,6 +83,52 @@ public class EnemyWaveController : MonoBehaviour
         {
             CancelInvoke();
             InvokeRepeating("MoveEnemyWave", 0.1f, 0.25f);
+        }
+    }
+
+    /// <summary>
+    /// Method that allows EnemyWaveController to obtain
+    /// JSON data from the LoadJSONData class
+    /// </summary>
+    public void GetJSONData(int enemies, int waves)
+    {
+        enemiesToSpawn = enemies;
+        wavesToSpawn = waves;
+    }
+    
+    /// <summary>
+    /// Method that limits the amount of enemies and waves to spawn,
+    /// see in method for specific reasons.
+    /// </summary>
+    private void LimitEnemiesAndWavesToSpawn()
+    {
+        // Limit enemies able to spawn to 5, due to screen space
+        if (enemiesToSpawn > 5)
+        {
+            enemiesToSpawn = 5;
+        }
+        
+        // Limit waves able to spawn to 10, due to performance
+        if (wavesToSpawn > 10)
+        {
+            wavesToSpawn = 10;
+        }
+    }
+
+    /// <summary>
+    /// Method that spawns the amount of waves and enemies per wave specified
+    /// in the JSON file
+    /// </summary>
+    private void SpawnWaves()
+    {
+        // Spawns enemies vertically from each necessary spawn point
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            for (int j = 0; j < wavesToSpawn; j++)
+            {
+                Instantiate(enemyToSpawn, new Vector3(spawnPoints[i].transform.position.x, spawnPoints[i].transform.position.y + (j * distanceBetweenWaves), 0),
+                        spawnPoints[i].transform.rotation, enemyWaveTransform);
+            }
         }
     }
 }
--- Assets/Code/LoadJSONData.cs
+++ Assets/Code/LoadJSONData.cs
@@ -3,19 +3,26 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 
+/// <summary>
+/// Class that loads data from JSON file, and then 
+/// stores that data to be used 
+/// </summary>
 public class LoadJSONData : MonoBehaviour
 {
-
     private string path;
     private string jsonString;
-    
-    
 
-    void Start ()
+    private EnemyWaveController enemyWaveController;
+
+    void Awake ()
     {
         path = Application.streamingAssetsPath + "/Wave.json";
         jsonString = File.ReadAllText(path);
 
         WaveData testWave = JsonUtility.FromJson<WaveData>(jsonString);
+        print(testWave.Enemies);
+
+        enemyWaveController = GetComponent<EnemyWaveController>();
+        enemyWaveController.GetJSONData(testWave.Enemies, testWave.Waves);
     }
 }
