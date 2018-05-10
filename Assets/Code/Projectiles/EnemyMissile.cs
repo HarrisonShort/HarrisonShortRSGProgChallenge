67cbba9a8bc36281fde242c214111f9c974f5fe9
Harrison Short <work.harrisonshort@gmail.com>
2018-05-11 00:38:42 +1000

    Added some class comments that were missing

--- Assets/Code/Projectiles/EnemyMissile.cs
+++ Assets/Code/Projectiles/EnemyMissile.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Class derived from AbstractProjectile for the 
+/// missiles that the enemies shoot
+/// </summary>
 public class EnemyMissile : AbstractProjectile
 {
     void Update ()
