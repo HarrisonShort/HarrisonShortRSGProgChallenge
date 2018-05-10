6d323ea95fadd1e4db766dd05e15848e9d5dbfbe
Harrison Short <work.harrisonshort@gmail.com>
2018-05-10 13:09:21 +1000

    Added class summary comment

--- Assets/Code/GameController.cs
+++ Assets/Code/GameController.cs
@@ -1,7 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
+/// <summary>
+/// Class that keeps track of player score and controls the current state of the game 
+/// (playing, game over, player wins), and then updates UI elements accordingly
+/// </summary>
 public class GameController : MonoBehaviour
 {
     
