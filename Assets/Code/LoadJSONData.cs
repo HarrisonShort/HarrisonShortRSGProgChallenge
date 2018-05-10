a998df3b11cdd5c96b260bb8cf26ea5e2f6b3806
Harrison Short <work.harrisonshort@gmail.com>
2018-05-10 21:59:00 +1000

    Created new classes to load json data for waves

---
+++ Assets/Code/LoadJSONData.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class LoadJSONData : MonoBehaviour
+{
+
+    private string path;
+    private string jsonString;
+    
+    
+
+    void Start ()
+    {
+        path = Application.streamingAssetsPath + "/Wave.json";
+        jsonString = File.ReadAllText(path);
+
+        WaveData testWave = JsonUtility.FromJson<WaveData>(jsonString);
+    }
+}
