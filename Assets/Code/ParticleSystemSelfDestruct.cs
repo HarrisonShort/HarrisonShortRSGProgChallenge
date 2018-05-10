32804b90bfc78c1223fc62edb91460ca8601dfdc
Harrison Short <work.harrisonshort@gmail.com>
2018-05-10 20:38:43 +1000

    Added class to auto destroy particle systems

---
+++ Assets/Code/ParticleSystemSelfDestruct.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Simple class that takes care of destroying particle systems
+/// when they have reached their respective durations
+/// </summary>
+public class ParticleSystemSelfDestruct : MonoBehaviour
+{
+    private ParticleSystem particleSystem;
+
+    void Start()
+    {
+        particleSystem = GetComponent<ParticleSystem>();
+    }
+
+    void Update()
+    {
+        if (particleSystem)
+        {
+            if (!particleSystem.IsAlive())
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
